98161de7a136c9ba2ced8316ae53c290449c860b
Charlie Hornsby <charlie.hornsby@hotmail.co.uk>
2020-08-30 18:02:28 +0300

    Use correct number of pixels

--- src/display.rs
+++ src/display.rs
@@ -1,12 +1,12 @@
 /// Stores the current lit state of every pixel on the Chip-8 display
 pub struct DisplayState {
-    pub pixels: [[bool; 640]; 320],
+    pub pixels: [[bool; 64]; 32],
 }
 
 impl Default for DisplayState {
     fn default() -> Self {
         Self {
-            pixels: [[false; 640]; 320],
+            pixels: [[false; 64]; 32],
         }
     }
 }
@@ -14,6 +14,6 @@ impl Default for DisplayState {
 impl DisplayState {
     /// Resets the display to a blank screen
     pub fn clear(&mut self) {
-        self.pixels = [[false; 640]; 320];
+        self.pixels = [[false; 64]; 32];
     }
 }
